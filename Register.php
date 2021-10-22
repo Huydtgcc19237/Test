2a2235df9e5ebff4614877c447b05ecea61367d0
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-10-22 16:23:06 +0700

    Update Register.php

--- Register.php
+++ Register.php
@@ -5,6 +5,7 @@
 <script src="js/jquery-3.2.0.min.js"></script>
 <script src="js/jquery.dataTables.min.js"></script>
 <script src="js/dataTables.bootstrap.min.js"></script>
+<div id="top">
  <?php
  if(isset($_POST['btnRegister']))
  {
@@ -31,7 +32,7 @@
          $err.="<li>Password must be greater than 5 characters</li>";
      }
      if($pass1!=$pass2){
-        $err.="<li>Password and Password confirm can not match,please check again.</li>";
+        $err.="<li>Password and confirm password are not the same</li>";
     }
     if($_POST['slYear']=="0"){
         $err.="<li>Choose Year of Birth, please</li>";
@@ -40,17 +41,17 @@
         echo $err;
     }
     else{
-        pg_connect("postgres://kpknfasetxhine:67da40fb98b4bb98a481cebe3383cabd50275b3ade3044685433cba0823fb8f5@ec2-3-213-41-172.compute-1.amazonaws.com:5432/d9dr4kegrianvb");
+        include_once("connection.php");
         $pass=md5($pass1);
-        $sq="select * from public.customer where Username='$us' or email='$email'";
-        $res=pg_query($conn,$sq);
+        $sq="select * from customer where Username='$us' or email='$email'";
+        $res=pg_query($sq);
         if(pg_num_rows($res)==0){
-            pg_query($conn,"Insert into customer (Username, Password, CustName,gender,Address,telephone,email, CusDate, CusMonth, CusYear,SSN,ActiveCode, state) values('$us','$pass','$fullname','$sex','$address','$tel','$email','$date','$month','$year','','',0)")
-            or die(pg_error($conn));
+            pg_query("Insert into customer (Username, Password, CustName,gender,Address,telephone,email, CusDate, CusMonth, CusYear,SSN,ActiveCode, state) values('$us','$pass','$fullname','$sex','$address','$tel','$email','$date','$month','$year','','',0)")
+            or die(pg_error());
             echo"You have registered successfully";
         }
         else{
-            echo"You loged in fail";
+            echo"Username or password already exists";
         }
     }
  }
@@ -60,69 +61,69 @@
 			 	<form id="form1" name="form1" method="post" action="" class="form-horizontal" role="form">
 					<div class="form-group">
 						    
-                            <label for="txtTen" class="col-sm-2 control-label">Username(*):  </label>
+                            <label for="txtTen" class="col-sm-2 control-label bre">Username(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtUsername" id="txtUsername" class="form-control" placeholder="Username" value=""/>
+							      <input type="text" name="txtUsername" id="txtUsername" class="form-control bre" placeholder="Username" value=""/>
 							</div>
                       </div>  
                       
                        <div class="form-group">   
-                            <label for="" class="col-sm-2 control-label">Password(*):  </label>
+                            <label for="" class="col-sm-2 control-label bre">Password(*):  </label>
 							<div class="col-sm-10">
-							      <input type="password" name="txtPass1" id="txtPass1" class="form-control" placeholder="Password"/>
+							      <input type="password" name="txtPass1" id="txtPass1" class="form-control bre" placeholder="Password"/>
 							</div>
                        </div>     
                        
                        <div class="form-group"> 
-                            <label for="" class="col-sm-2 control-label">Confirm Password(*):  </label>
+                            <label for="" class="col-sm-2 control-label bre">Confirm Password(*):  </label>
 							<div class="col-sm-10">
-							      <input type="password" name="txtPass2" id="txtPass2" class="form-control" placeholder="Confirm your Password"/>
+							      <input type="password" name="txtPass2" id="txtPass2" class="form-control bre" placeholder="Confirm your Password"/>
 							</div>
                        </div>     
                        
                        <div class="form-group">                               
-                            <label for="lblFullName" class="col-sm-2 control-label">Full name(*):  </label>
+                            <label for="lblFullName" class="col-sm-2 control-label bre">Full name(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtFullname" id="txtFullname" value="" class="form-control" placeholder="Enter Fullname"/>
+							      <input type="text" name="txtFullname" id="txtFullname" value="" class="form-control bre" placeholder="Enter Fullname"/>
 							</div>
                        </div> 
                        
                        <div class="form-group">      
-                            <label for="lblEmail" class="col-sm-2 control-label">Email(*):  </label>
+                            <label for="lblEmail" class="col-sm-2 control-label bre">Email(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtEmail" id="txtEmail" value="" class="form-control" placeholder="Email"/>
+							      <input type="text" name="txtEmail" id="txtEmail" value="" class="form-control bre" placeholder="Email"/>
 							</div>
                        </div>  
                        
                         <div class="form-group">   
-                             <label for="lblDiaChi" class="col-sm-2 control-label">Address(*):  </label>
+                             <label for="lblDiaChi" class="col-sm-2 control-label bre">Address(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtAddress" id="txtAddress" value="" class="form-control" placeholder="Address"/>
+							      <input type="text" name="txtAddress" id="txtAddress" value="" class="form-control bre" placeholder="Address"/>
 							</div>
                         </div>  
                         
                          <div class="form-group">  
-                            <label for="lblDienThoai" class="col-sm-2 control-label">Telephone(*):  </label>
+                            <label for="lblDienThoai" class="col-sm-2 control-label bre">Telephone(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtTel" id="txtTel" value="" class="form-control" placeholder="Telephone" />
+							      <input type="text" name="txtTel" id="txtTel" value="" class="form-control bre" placeholder="Telephone" />
 							</div>
                          </div> 
                          
                           <div class="form-group">  
-                            <label for="lblGioiTinh" class="col-sm-2 control-label">Gender(*):  </label>
-							<div class="col-sm-10">                              
-                                      <label class="radio-inline"><input type="radio" name="grpRender" value="0" id="grpRender"  />
+                            <label for="lblGioiTinh" class="col-sm-2 control-label bre">Gender(*):  </label>
+							<div class="col-sm-10 bre">                              
+                                      <label class="radio-inline bre"><input type="radio" name="grpRender" value="0" id="grpRender"  />
                                       Male</label>
                                     
-                                      <label class="radio-inline"><input type="radio" name="grpRender" value="1" id="grpRender" />
+                                      <label class="radio-inline bre"><input type="radio" name="grpRender" value="1" id="grpRender" />
                                       
                                       Female</label>
 							</div>
                           </div> 
                           
-                          <div class="form-group"> 
+                          <div class="form-group bre"> 
                             <label for="lblNgaySinh" class="col-sm-2 control-label">Date of Birth(*):  </label>
-                            <div class="col-sm-10 input-group">
+                            <div class="col-sm-10 input-group bre">
                                 <span class="input-group-btn">
                                   <select name="slDate" id="slDate" class="form-control" >
                 						<option value="0">Choose Date</option>
@@ -160,13 +161,13 @@
                            </div>
                       </div>	
 					<div class="form-group">
-						<div class="col-sm-offset-2 col-sm-10">
+						<div class="col-sm-10">
 						      <input type="submit"  class="btn btn-primary" name="btnRegister" id="btnRegister" value="Register"/>
-                              <input type="button" class="btn btn-primary" name="btnCancel"  id="btnCancel" value="Cancel" onclick="window.location='index.php'" />
-                              	
+                              <input type="button" name="btnCancel"  class="btn btn-primary bre" id="btnCancel" value="Cancel" onclick="window.location='index.php'" />	
 						</div>
                      </div>
 				</form>
+</div>
 </div>
     
 
