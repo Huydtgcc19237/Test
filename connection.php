0e30868b6c437037200bf99d6f9d020fbbf81feb
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-09 15:40:14 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-	$Conn = pg_connect("postgres://kpknfasetxhine:67da40fb98b4bb98a481cebe3383cabd50275b3ade3044685433cba0823fb8f5@ec2-3-213-41-172.compute-1.amazonaws.com:5432/d9dr4kegrianvb");
-    if (!$Conn) {
+	$Connect = pg_connect("postgres://kpknfasetxhine:67da40fb98b4bb98a481cebe3383cabd50275b3ade3044685433cba0823fb8f5@ec2-3-213-41-172.compute-1.amazonaws.com:5432/d9dr4kegrianvb");
+    if (!$Connect) {
         die("Connection failed");
     }
 ?>
