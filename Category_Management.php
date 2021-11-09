0c1f8242248f43ae62b477e00d823589caabd8a1
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-09 14:47:31 +0700

    Update Category_Management.php

--- Category_Management.php
+++ Category_Management.php
@@ -45,13 +45,14 @@
             <?php
             $No=1;
             $result=pg_query("select * from category");
+                    var_dump($result);
             while($row=pg_fetch_row($result))
             {
             ?>
 			<tr>
               <td class="cotCheckBox"><?php echo $No;?></td>
-              <td><?php echo $row[1];?></td>
-              <td><?php echo $row[2];?></td>
+              <td><?php echo $row['cat_name'];?></td>
+              <td><?php echo $row['cat_des'];?></td>
               <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["cat_id"];?>">
               <img src='images/edit.png' border='0'  /></a></td>
               <td style='text-align:center'><a href="?page=category_management&&function=del&&id=<?php echo $row["cat_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
