28da1cc9826fb03548d7c8298b6b3ef8e9c5d51e
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-09 15:37:47 +0700

    Update Add_Category.php

--- Add_Category.php
+++ Add_Category.php
@@ -24,11 +24,11 @@
 			$id=htmlspecialchars(pg_escape_string($conn,$id));
 			$name=htmlspecialchars(pg_escape_string($conn,$name));
 			$des=htmlspecialchars(pg_escape_string($conn,$des));
-			$sq="Select * from category where Cat_ID='$id' or Cat_Name='$name'";
+			$sq="Select * from category where cat_id='$id' or cat_id='$name'";
 			$result=pg_query($conn,$sq);
 			if(pg_num_rows($result)==0)
 			{
-				pg_query($conn,"INSERT INTO category (cat_id, cat_name,cat_dec)
+				pg_query($conn,"INSERT INTO category (cat_id, cat_name,cat_dess)
 				VALUES ('$id','$name','$des')");
 				echo '<meta http-equiv="refresh" content="0,URL=?page=category_management"/>';
 			}
