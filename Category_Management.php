92d148da6201e61f39c8823e4e444ff32e9dedef
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-09 13:47:54 +0700

    Update Category_Management.php

--- Category_Management.php
+++ Category_Management.php
@@ -17,7 +17,7 @@
         if(isset($_GET["function"])=='del'){
             if(isset($_GET["id"])){
                 $id=$_GET["id"];
-                pg_query("delete from category where Cat_ID='$id'");
+                pg_query($conn,"delete from category where Cat_ID='$id'");
             }
         }
         ?>
@@ -41,7 +41,7 @@
 			<tbody>
             <?php
             $No=1;
-            $result=pg_query("SELECT * FROM category1");
+            $result=pg_query($conn,"Select * from category");
             while($row=pg_fetch_array($result))
             {
             ?>
@@ -49,9 +49,9 @@
               <td class="cotCheckBox"><?php echo $No;?></td>
               <td><?php echo $row["Cat_Name"];?></td>
               <td><?php echo $row["Cat_Des"];?></td>
-              <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["cat_id"];?>">
+              <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["Cat_ID"];?>">
               <img src='images/edit.png' border='0'  /></a></td>
-              <td style='text-align:center'><a href="?page=category_management&&function=del&&id=<?php echo $row["cat_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
+              <td style='text-align:center'><a href="?page=category_management&&function=del&&id=<?php echo $row["Cat_ID"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
             </tr>
             <?php $No++;}?>
 			</tbody>
