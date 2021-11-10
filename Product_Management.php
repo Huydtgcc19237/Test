c2095fd12fc79ba91ecf81ea9d610a4a0a132f5e
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-10 19:14:49 +0700

    Update Product_Management.php

--- Product_Management.php
+++ Product_Management.php
@@ -1,10 +1,19 @@
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <div id="top">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script>
+    function deleteConfirm(){
+        if(confirm("Are you sure to delete!")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    </script>
         <form name="frm" method="post" action="">
         <h1>Product Management</h1>
         <p>
-        	<a href="?page=add_product" ><img src="images/add.png" alt="" width="16" height="16" border="0" /> Add new</a>
+        	<img src="images/add.png" alt="Thêm mới" width="16" height="16" border="0" /><a href="?page=add_product">Add new</a>
         </p>
         <table id="tableproduct" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
@@ -22,58 +31,44 @@
              </thead>
 
 			<tbody>
-            <script language="javascript">
-            function deleteConfirm(){
-                if(confirm("Are you sure to delete!")){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            </script>
             <?php
             include_once("connection.php");
             if(isset($_GET["function"])=="del"){
                 if(isset($_GET["id"])){
                     $id=$_GET["id"];
-                    $sq="select pro_image from product where Pro_ID='$id'";
-                    $res=pg_query($sq);
+                    $sq="SELECT pro_image FROM product WHERE Pro_ID='$id'";
+                    $res=pg_query($conn,$sq);
                     $row=pg_fetch_array($res);
                     $filePic=$row['Pro_image'];
-                    unlink("image/".$filePic);
-                    pg_query("DELETE FROM product WHERE Pro_ID='$id'");
+                    unlink("images/".$filePic);
+                    pg_query("delete from product where Pro_ID='$id'");
                 }
-            }   
+            }
             ?>
-                <?php
-                $No=1;
-                $result=pg_query("SELECT product.pro_id, product.pro_name, product.pro_price, product.pro_qty, product.pro_image, category.cat_id,
-                from product, category where product.cat_id = category.cat_id");
-                while($row=pg_fetch_array($result))
-                {      
-                ?>
+            
+            <?php
+			$No=1;
+            $result=pg_query("SELECT product.pro_id, product.pro_name, product.pro_price, product.pro_qty, product.pro_image, category.cat_id,
+            from product, category where product.cat_id = category.cat_id");
+            while($row=pg_fetch_array($result)){	
+			?>
 			<tr>
               <td ><?php echo $No; ?></td>
-              <td ><?php echo $row['pro_id']; ?></td>
-              <td><?php echo $row['pro_name']; ?></td>
-              <td><?php echo $row['pro_price']; ?></td>
-              <td ><?php echo $row['pro_qty']; ?></td>
-              <td><?php echo $row['cat_id']; ?></td>
-              <td><?php echo $row['shop_id']; ?></td>
+              <td ><?php echo $row["Product_ID"];  ?></td>
+              <td><?php echo $row["Product_Name"];  ?></td>
+              <td><?php echo $row["Price"];   ?></td>
+              <td ><?php echo $row["Pro_qty"]; ?></td>
+              <td><?php echo $row["Cat_id"]; ?></td>
              <td align='center' class='cotNutChucNang'>
-                 <img src='images/<?php echo $row['pro_image']; ?>' border='0' width="50" height="50"  /></td>
-             <td align='center'><a href="?page=update_product&&id=<?php echo $row["pro_id"]; ?>"><img src='images/edit.png' border='0'/></a></td>
-             <td align='center'><a href="?page=product_management&&function=del&&id=<?php echo $row["pro_id"]; ?>" onclick="return deleteConfirm()">
-              <img src='images/delete.png' border='0' /></td>
+                 <img src='images/<?php echo $row['Pro_image'] ?>' border='0' width="50" height="50"  /></td>
+             <td align='center' class='cotNutChucNang'><a href="?page=update_product&&id=<?php echo $row["Product_ID"];?>"><img src='images/edit.png' border='0'/></a></td>
+             <td align='center' class='cotNutChucNang'><a href="?page=product_management&&function=del&&id=<?php echo $row["Product_ID"];?>"onclick="return deleteConfirm()"><img src='images/delete.png' border='0' /></a></td>
             </tr>
             <?php
-            $No++;
-                }
+               $No++;
+            }
 			?>
 			</tbody>
         
         </table>  
-
  </form>
-            </div>
