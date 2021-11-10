6df41e7ee296b5d0ed523e97f788f00d6c984bde
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-10 19:16:05 +0700

    Update Product_Management.php

--- Product_Management.php
+++ Product_Management.php
@@ -50,7 +50,8 @@
 			$No=1;
             $result=pg_query("SELECT product.pro_id, product.pro_name, product.pro_price, product.pro_qty, product.pro_image, category.cat_id,
             from product, category where product.cat_id = category.cat_id");
-            while($row=pg_fetch_array($result)){	
+            while($row=pg_fetch_array($result)){
+                var_dump($result)	
 			?>
 			<tr>
               <td ><?php echo $No; ?></td>
