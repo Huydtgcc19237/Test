08b634864e8268e34599775cea714f71ce60eac4
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-11 09:14:59 +0700

    Update manager_feedback.php

--- manager_feedback.php
+++ manager_feedback.php
@@ -8,11 +8,12 @@
         <img src="images/add.png" alt="Add new" width="16" height="16" border="0" /> 
         <a href="?page=add_fb"> Add</a>
     </p>
-    <table id="tablecategory" class="table table-striped table-bordered" cellspacing="0" width="100%">
+    <table id="tablefeedback" class="table table-striped table-bordered" cellspacing="0" width="100%">
         <thead>
             <tr>
                 <th style='text-align:center'><strong>No.</strong></th>
-                <th style='text-align:center'><strong>Email of username</strong></th>
+                <th style='text-align:center'><strong>Email</strong></th>
+                <th style='text-align:center'><strong>Username</strong></th>
                 <th style='text-align:center'><strong>Small Feedback</strong></th>
                 <th style='text-align:center'><strong>Detail Feedback</strong></th>
             </tr>
@@ -30,10 +31,11 @@
                     <tr>
                         <td style='text-align:center'><?php echo $No;?></td>
                         <td><?php echo $row["email"];?></td>
+                        <td><?php echo $row["username"];?></td>
                         <td><?php echo $row["fb_smalldes"];?></td>
                         <td><?php echo $row["fb_des"];?></td>
-                        <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["fb_id"];?>"><img src='images/edit.png' border='0'  /></a></td>
-                        <td style='text-align:center'><a href="?page=manager_feedback&&function=del&&id=<?php echo $row["fb_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
+                        <td style='text-align:center'><a href="?page=update_fb&&id=<?php echo $row["fb_id"];?>"><img src='images/edit.png' border='0'  /></a></td>
+                        <td style='text-align:center'><a href="?page=manager_fb&&function=del&&id=<?php echo $row["fb_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
             <?php
                 $No++;
                 }
