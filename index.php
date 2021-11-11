71f5407f8f03991a1be602cc84276013dd63d6bb
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-11 14:31:14 +0700

    a

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Maveriktoy.com</title>
+    <title>ATNSHOP</title>
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
