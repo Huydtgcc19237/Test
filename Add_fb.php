4d0e62191518cb5ccf809f6f6517fcaa72a3491e
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-11 08:42:42 +0700

    Update Add_fb.php

--- Add_fb.php
+++ Add_fb.php
@@ -6,12 +6,12 @@
         <?php
 	include_once("connection.php");
     function bind_Username_List(){
-        $sqlstring ="select username from customer";
+        $sqlstring ="select username , email from customer";
         $result =pg_query($sqlstring);
-        echo "<select name='UsernameList' class='from-control'>
+        echo "<select name='CategoryList' class='from-control'>
         <option value='0'>Choose username</option>";
         while($row=pg_fetch_array($result)){
-            echo "<option value='".$row['username']."'</option>";
+            echo "<option value='".$row['username']."'>".$row['email']."</option>";
         }
         echo "</select>";
     }
@@ -24,19 +24,19 @@
 		if($id==""){
 			$err.="<li>Enter Feeback ID, please</li>";
 		}
-		if($name==""){
+		if($username==""){
 			$err.="<li>Enter choose username, please</li>";
 		}
 		if($err!=""){
 			echo"<ul>$err</ul>";
 		}
 		else{
-			$sq=" Select * from feedback where fb_id='$id' or username='$name'";
-			$result=pg_query($sq);
+			$sq=" Select * from feedback where fb_id='$id' or username='$username'";
+			$result=pg_query($Connect,$sq);
 			if(pg_num_rows($result)==0)
 			{
 				pg_query("INSERT INTO feedback (fb_id, username, fb_smalldes, fb_des)
-				VALUES ('$id','$name','$smalldes','$des')");
+				VALUES ('$id','$username','$smalldes','$des')");
 				echo '<meta http-equiv="refresh" content="0,URL=?page=manager_feedback"/>';
 			}
 			else
@@ -51,7 +51,7 @@
         ?>
 
 <div class="container">
-	<h2>Adding Category</h2>
+	<h2>Adding Feeback</h2>
 			 	<form id="form1" name="form1" method="post" action="" class="form-horizontal" role="form">
 				 <div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Feedback ID(*):  </label>
@@ -59,7 +59,7 @@
 							      <input type="text" name="txtID" id="txtID" class="form-control" placeholder="Feedback ID" value='<?php echo isset($_POST["txtID"])?($_POST["txtID"]):"";?>'>
 							</div>
 					</div>	
-				 <div class="form-group">   
+                <div class="form-group">   
                     <label for="" class="col-sm-2 control-label">Username(*):  </label>
 							<div class="col-sm-10">
 							      <?php bind_Username_List();  ?>
