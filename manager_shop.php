3ff8a077c762a7b4eaf8a7047f505a8f291c896e
Huydtgcc19237 <91666402+Huydtgcc19237@users.noreply.github.com>
2021-11-11 19:59:49 +0700

    Update manager_shop.php

--- manager_shop.php
+++ manager_shop.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" type="text/css" href="style.css"/>
+  <!-- Bootstrap --> 
+  <link rel="stylesheet" type="text/css" href="style.css"/>
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="css/bootstrap.min.css">
         <script>
@@ -23,46 +24,45 @@
             }
         }
         ?>
-<?php
-    if(isset($_SESSION['us']) && $_SESSION['admin']==1)
-    {
-?>
-<form name="frm" method="post">
-    <h2 style="text-align: center;">Management Shop</h2>
-    <table id="tablecategory" class="table table-striped table-bordered" cellspacing="0" width="100%">
-        <thead>
+        <form name="frm" method="post" action="">
+        <h1>Management Shop</h1>
+        <p>
+        <img src="images/add.png" alt="Add new" width="16" height="16" border="0" /> 
+        <a href="?page=add_shop"> Add</a>
+        </p>
+        <table id="tablecategory" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
             <tr>
                 <th style='text-align:center'><strong>No.</strong></th>
-                <th style='text-align:center'><strong>ID Shop</strong></th>
                 <th style='text-align:center'><strong>Name Shop</strong></th>
                 <th style='text-align:center'><strong>Shop Address</strong></th>
             </tr>
-        </thead>
+             </thead>
 
-		<tbody>
+			<tbody>
             <?php
-                include_once("connection.php");
-                $No=1;
-                $result = pg_query( "SELECT * FROM shop ") 
-                        or die(pg_error());
-                while($row=pg_fetch_array($result))
-                {
+            $No=1;
+            $result=pg_query("select * from shop");
+            while($row=pg_fetch_array($result))
+            {
             ?>
-                    <tr>
-                        <td style='text-align:center'><?php echo $No;?></td>
-                        <td><?php echo $row["shop_id"];?></td>
-                        <td><?php echo $row["shop_name"];?></td>
-                        <td><?php echo $row["shop_loca"];?></td>
-                        <td style='text-align:center'><a href="?page=manager_shop&&function=del&&id=<?php echo $row["cat_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
+			<tr>
+              <td class="cotCheckBox"><?php echo $No;?></td>
+              <td><?php echo $row["shop_name"];?></td>
+              <td><?php echo $row["shop_loca"];?></td>
+              <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["shop_id"];?>"><img src='images/edit.png' border='0'  /></a></td>
+              <td style='text-align:center'><a href="?page=category_management&&function=del&&id=<?php echo $row["shop_id"];?>" onclick="deleteConfirm()"><img src='images/delete.png' border='0'></a></td>
+            </tr>
             <?php
-                $No++;
-                }
-            ?>
-		</tbody>
-    </table>  
-</form>
-<?php
-    }
-?>
-<br>
-<br>
+             $No++;}
+             ?>
+			</tbody>
+        </table>  
+        
+        <!--Nút Thêm mới , xóa tất cả-->
+        <div class="row" style="background-color:#FFF"><!--Nút chức nang-->
+            <div class="col-md-12">
+            	
+            </div>
+        </div><!--Nút chức nang-->
+ </form>
